2868dfe7b8be2ec2d8da6df972a55c7c32f18d23
Anish-Dey <Anish-Dey>
2024-09-25 09:32:30 +0000

    update class Bronze

--- 04-bronze.py
+++ 04-bronze.py
@@ -35,6 +35,93 @@ class Bronze():
                     
         spark.sparkContext.setLocalProperty("spark.scheduler.pool", "bronze_p2")
         if once == True:
+            return stream_writer.trigger(availableNow=True).toTable(f"{self.catalog}.{self.db_name}.registered_users_bz")
+        else:
+            return stream_writer.trigger(processingTime=processing_time).toTable(f"{self.catalog}.{self.db_name}.registered_users_bz")
+        
+    def consume_gym_logins(self, once= True, processing_time="5 seconds"):
+        import pyspark.sql.functions as F
+        schema = "mac_address string, gym bigint, login double, logout double"
+
+        df_stream = (spark.readStream
+                          .format("cloudFiles")
+                          .schema(schema)
+                          .option("cloudFiles.format", "csv")
+                          .option("header", "true")
+                          .load(self.landing_zone + "/gym_logins_bz")
+                          .withColumn("load_time", F.current_timestamp())
+                          .withColumn("source_file", F.input_file_name())
+        )
+        # Use append mode because bronze layer is expected to insert only from source
+        stream_writer = df_stream.writeStream \
+                                 .format("delta") \
+                                 .option("checkpointLocation", self.checkpoint_base + "/gym_logins_bz") \
+                                 .outputMode("append") \
+                                 .queryName("gym_logins_bz_ingestion_stream")
+        
+        spark.sparkContext.setLocalProperty("spark.scheduler.pool", "bronze_p2")
+
+        if once == True:
             return stream_writer.trigger(availableNow=True).toTable(f"{self.catalog}.{self.db_name}.gym_logins_bz")
         else:
             return stream_writer.trigger(processingTime=processing_time).toTable(f"{self.catalog}.{self.db_name}.gym_logins_bz")
+        
+    def consume_kafka_multiplexed(self,once=True, processing_time="5 seconds"):
+        import pyspark.sql.functions as F
+        schema = "key string, value string, topic string, partition bigint, offset bigint, timestamp bigint"
+
+        df_stream = (spark.readStream
+                          .format("cloudfiles")
+                          .schema(schema)
+                          .option("maxFilesPerTrigger", 1)
+                          .option("cloudFiles.format", "json")
+                          .load(self.landing_zone + "/kafka_multiplex_bz")
+                          .withColumn("load_time", F.current_timestamp())
+                          .withColumn("source_file", F.input_file_name())
+                          .join(F.broadcast(df_date_lookup)
+                                , [F.to_date((F.col("timestamp")/1000).cast("timestamp")) == F.col("date")]
+                                , "left")
+                )
+        # Use append mode because bronze layer is expected to insert only from source
+        stream_writer = df_stream.writeStream \
+                                 .format("delta") \
+                                 .option("checkpointLocation", self.checkpoint_base + "/kafka_multiplex_bz") \
+                                 .outputMode("append") \
+                                 .queryName("kafka_multiplex_bz_ingestion_stream")
+        
+        spark.sparkContext.setLocalProperty("spark.scheduler.pool", "bronze_p1")
+        if once == True:
+            return stream_writer.trigger(availableNow=True).toTable(f"{self.catalog}.{self.db_name}.kafka_multiplex_bz")
+        else:
+            return stream_writer.trigger(processingTime=processing_time).toTable(f"{self.catalog}.{self.db_name}.kafka_multiplex_bz")
+    
+    def consume(self, once=True, processing_time = "5 seconds"):
+        import time
+        start = int(time.time())
+        print(f"\nStarting Bronze Layer Consumption")
+        self.consume_user_registration(once, processing_time)
+        self.consume_gym_logins(once, processing_time)
+        self.consume_kafka_multiplexed(once, processing_time)
+
+        if once:
+            for stream in spark.streams.active:
+                stream.awaitTermination()
+        
+        print(f"Completed bronze layer consumtion {int(time.time()) - start} seconds")
+    
+    def assert_count(self, table_name, expected_count, filter = "true"):
+        print(f"Validating record counts in {table_name}...", end='')
+        actual_count = spark.read.table(f"{self.catalog}.{self.db_name}.{table_name}").filter(filter).count()
+        assert actual_count == expected_count, f"Expected {expected_count: ,} records, found {actual_count:, } in {table_name} where {filter}"
+        print(f"Found {actual_count:,} / Expected {expected_count:,} records where {filter}: Success")        
+
+    def validate(self, sets):
+        import time
+        start = int(time.time())
+        print(f"\nValidating bronz layer records...")
+        self.assert_count("registered_users_bz", 5 if sets == 1 else 10)
+        self.assert_count("gym_logins_bz", 8 if sets == 1 else 16)
+        self.assert_count("kafka_multiplex_bz", 7 if sets == 1 else 13, "topic='user_info'")
+        self.assert_count("kafka_multiplex_bz", 16 if sets == 1 else 32, "topic='workout'")
+        self.assert_count("kafka_multiplex_bz", sets * 253801, "topic='bpm'")
+        print(f"Bronze layer validation completed in {int(time.time()) - start} seconds")                
